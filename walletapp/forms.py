6c1e1acc3586eb5a8dc43ef84cb9e043cf5dba29
Mercy-Jepchirchir <mjepchirchirkemboi@gmail.com>
2022-09-01 17:27:27 +0300

    added a form of the models and views with the path

---
+++ walletapp/forms.py
@@ -0,0 +1,52 @@
+from django import forms
+from .models import Account, Card, Currency, Customer, Loan, Notification, Receipt, ThirdParty, Wallet
+
+class CustomerRegistrationForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = "__all__"
+        
+class CurrencyRegistrationForm(forms.ModelForm):
+    class Meta:
+        model = Currency
+        fields = "__all__"
+
+class WalletRegistrationForm(forms.ModelForm):
+    class Meta:
+        model = Wallet
+        fields = "__all__"
+        
+class AccountRegistrationForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        fields = "__all__"
+        
+class TransactionRegistrationForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        fields = "__all__"
+        
+class CardRegistrationForm(forms.ModelForm):
+    class Meta:
+        model = Card
+        fields = "__all__"
+        
+class ThirdpartyRegistrationForm(forms.ModelForm):
+    class Meta:
+        model = ThirdParty
+        fields = "__all__"
+        
+class NotificationRegistrationForm(forms.ModelForm):
+    class Meta:
+        model = Notification
+        fields = "__all__"
+        
+class ReceiptRegistrationForm(forms.ModelForm):
+    class Meta:
+        model = Receipt
+        fields = "__all__"
+
+class LoanRegistrationForm(forms.ModelForm):
+    class Meta:
+        model = Loan
+        fields = "__all__"
---
+++ walletapp/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import register_thirdparty,register_notification,register_customer,register_currency,register_wallet,register_account,register_transaction,register_card,register_receipt,register_loan
+
+
+urlpatterns = [
+    path("register/",register_customer,name="registration"),
+    path("currency/",register_currency,name="currency"),
+    path("wallet/",register_wallet,name="wallet"),
+    path("account/",register_account,name="Account"),
+    path("transaction/",register_transaction, name="transaction"),
+    path("card/",register_card,name="card"),
+    path("thirdparty/",register_thirdparty,name="thirdparty"),
+    path("notification/",register_notification,name="Notification"),
+    path("receipt/",register_receipt,name="receipt"),
+    path("loan/",register_loan,name="loan"),
+    
+    
+]
